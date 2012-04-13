3b76989c4037a4065ccc378440652651f6a4580c
mmerinovega@gmail.com <mmerinovega@gmail.com@93503632-92bd-740b-9f91-4bc13b9d65ea>
2012-04-13 06:15:30 +0000

    Actualizado con todo lo avanzado en el curso.

--- GFive/GF.Portal/GF.Layer.BL.BC/Rpedido.vb
+++ GFive/GF.Portal/GF.Layer.BL.BC/Rpedido.vb
@@ -40,4 +40,41 @@
         Return resultado
     End Function
 
+    Public Function ActualizarSituacion(ByVal BE_Pedido As Epedido) As Integer()
+        Dim resultado() As Integer
+
+        Dim options As New TransactionOptions
+        options.IsolationLevel = Transactions.IsolationLevel.ReadCommitted
+        options.Timeout = New TimeSpan(0, 2, 0)
+
+        Using scope As New TransactionScope(TransactionScopeOption.Required, options)
+            Try
+                resultado = Dpedido.ActualizarSituacion(BE_Pedido)
+                scope.Complete()
+            Catch ex As Exception
+                Throw
+            End Try
+        End Using
+        Return resultado
+
+    End Function
+
+    Public Function ListadoPedido() As List(Of PedidoRest)
+        Dim resultado As New List(Of PedidoRest)
+        Dim options As New TransactionOptions
+        options.IsolationLevel = Transactions.IsolationLevel.ReadCommitted
+        options.Timeout = New TimeSpan(0, 2, 0)
+
+        Using scope As New TransactionScope(TransactionScopeOption.Required, options)
+            Try
+                resultado = Dpedido.ListadoPedido()
+                scope.Complete()
+            Catch ex As Exception
+                Throw
+            End Try
+        End Using
+        Return resultado
+
+    End Function
+
 End Class
